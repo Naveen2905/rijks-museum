c2b72dd20f300a6dd4df03805f12872b36f2c7b2
Naveen2905 <naveenmalhotra05@gmail.com>
2020-02-27 12:39:47 -0500

    Updates

---
+++ precache-manifest.d547d10e9a498ccff1cb77be3c736c47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a550ebbd1a5d98de5807f79487aff64",
+    "url": "/rijks-museum/index.html"
+  },
+  {
+    "revision": "6d4203e7c878fceda6f0",
+    "url": "/rijks-museum/static/css/main.a3e90b1c.chunk.css"
+  },
+  {
+    "revision": "e71af9764a80be366313",
+    "url": "/rijks-museum/static/js/2.e25c129a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/rijks-museum/static/js/2.e25c129a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d4203e7c878fceda6f0",
+    "url": "/rijks-museum/static/js/main.319b213f.chunk.js"
+  },
+  {
+    "revision": "4c23af9206ffabaf568e",
+    "url": "/rijks-museum/static/js/runtime-main.1648a019.js"
+  }
+]);
